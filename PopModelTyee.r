ac9fce1823db701a8aaa76c6536bc98da7c3ff3d
Brian Davis <broadwaybrian@gmail.com>
2020-02-11 15:10:30 -0800

    Update PopModelTyee.r

--- PopModelTyee.r
+++ PopModelTyee.r
@@ -2,114 +2,77 @@ source("code/functions.r")
 ##################
 ### Parameters ###
 ##################
-m <- matrix(c(0,.425,0,1.5,0,.75,2.3,0,.035),ncol = 3)
-st <- c(3000,2000,1000) # Starting abundance - 0,1,ad
-n <- 50 # rows per age class - different threads of stochasticity
-z <- 100 # number of matrices - years into future
-###################
-### Suppression ###
-###################
-sup <- c(.3,.5,.4) # vector == proprtion of fish suppressed, age 0 ,1, and 2
-sdd <- .024 # standard deviation of suppression rates
-# sapply(1 - sup,function(x) rbeta(n,beta.mom(x,sdd)[1],beta.mom(x,sdd)[2]))
-CE <- function(x,top,b) (1-exp(-b*x))*top # capture efficiency as a f() of abundance
-SuppressionRateDecay <- .0005 # try b = .003,.008, .001, and .0005
-curve(CE(x,top = .3,SuppressionRateDecay),0,6000,lwd = 3)
-################
-### YY Males ###
-################
-# Sex Ratio
+
+n <- 10 # rows per age class - different threads of stochasticity
+z <- 50 # number of matrices - years into future
 Sex <- .5 # proportion of females - natural population
+YYS <- .18 # YY survival from age 0 to spawn
+YYN <- 3000
+# YYS0 <- 0
 #
-Nstock0 <- 1000
-Nstock1 <- 3000 # stocking YY males age 1
-Nstock2 <- 2000
-# Kennedy et al.
-YYS <- .18 # YYmale survival
-YYfitness <- .58 # spawning Fitness
+m <- matrix(c(0,.425,0,1.5,0,.75,2.3,0,.035),ncol = 3) # Male survival matrix
+mM <- matrix(c(0,.425,0,0,0,.75,0,0,.035),ncol = 3) # Male survival matrix
+mF <- matrix(c(0,.425,0,3,0,.75,4.6,0,.035),ncol = 3) # Female
+mYY <- matrix(c(0,.18,0,0,0,.25,0,0,.09),ncol = 3)
+st <- c(3000,2000,1000) # Starting abundance - 0,1,ad
+stF <- matrix(rep(c(3000,2000,1000)/2,n,each = n),n,3)
+stM <- matrix(rep(c(3000,2000,1000)/2,n,each = n),n,3)
+stYY <- cbind(rbinom(n = n,size = YYN,prob = YYS),0,0) # Starting abundance - 0,1,ad
+ST <- cbind(stF,stM,stYY,NA,NA,NA,NA,NA)
+S <- c(.425,.75,.035,.425,.75,.035,1,1,1)
+SYY <- c(.18,.18,.18,.18)
+Fc <- c(mF[1,])
 
-# messing
-Nstock0*YYS
+# Try this again with stoch atart
+out <- array(0,dim = c(n,9,z),dimnames = list(NULL,c("F0","F1","Fad","M0","M1","Mad","YY0","YY1","YYad"),paste0("T",1:z))) #,"Fn","Mn","YYn","N","Sex"
+out[,,1] <- cbind(stF,stM,stYY)
+# Add Stoch
+out[,c(1,4),1] <- apply(out[,c(1,4),1],2,rpois,n=n)
+out[,1:6,1] <- round(f_suppress(out[,1:6,1],sdd,n = n,sup = sup) * out[,1:6,1])
 
+cbind(rbinom(n = n,size = YYN,prob = YYS),0,0)
+#
+# f <- function(st,n,S,Fc){
+# a <- rpois(n,Fc %*% st)
+# b <- matrix(rbinom(n*3,st,prob = S),n,3,byrow = T)
+# cbind(a,b[,1],apply(b[,2:3],1,sum))
+# }
+# f(st,n,S,Fc)
 
+st <- out[,1:9,1]
+YYS <- SYY
 
-#############
-### Model ###
-#############
-# # for diagnoses
-mat <- m
-vec <- matrix(st,1,3)
-# vec <- structure(c(12, 5, 16, 12, 2, 11, 6, 6, 20, 7, 2, 1, 3, 1, 1, 
-#                    3, 2, 1, 4, 4, 2, 0, 0, 1, 0, 2, 0, 1, 1, 1), .Dim = c(10L, 3L
-#                    ), .Dimnames = list(NULL, c("0", "1", "ad")))
-dsts <- list("rpois","rpois","rbinom","rbinom","rbinom")
-g <- function(n,vec,mat) {
-  args <- list(list("n" = n,"lambda" = vec[,2]*mat[1,2]),
-               list("n" = n,"lambda" = vec[,3]*mat[1,3]),
-               list("n" = n,"size" = vec[,1],"prob" = mat[2,1] ),
-               list("n" = n,"size" = vec[,2],"prob" = mat[3,2] ),
-               list("n" = n,"size" = vec[,3],"prob" = mat[3,3] ))
-  tmp <- mapply(dsts,args,FUN = do.call) # columns = "age" 0 (from 1s), 0 (from ADs), 1s, ADs (2s), ADs
-  tmp <- cbind(sapply(list(1:2,4:5),function(x) apply(tmp[,x],1,sum)),tmp[,3])[,c(1,3,2)]
-  
-  tmp2 <- 1-sapply(1:3,function(x) CE(tmp[,x],top = sup[x],b = SuppressionRateDecay)) # Capture efficiency base on curve
-  a <- apply(tmp2,c(1,2),function(x) beta.mom(x,sd = sdd)[1])
-  b <- apply(tmp2,c(1,2),function(x) beta.mom(x,sd = sdd)[2])
-  tmp2 <- matrix(rbeta(tmp2,a,b),n,3)
-  # round(tmp %*% diag(sup)) # removal
-  round(tmp*tmp2)
+f <- function(st,n,S,Fc,YYS){
+  # Matrix Math w/ stochasticity
+  a <- sapply(1:n,function(x) rpois(1,Fc %*% st[x,1:3]))
+  a2 <- t(sapply(1:nrow(st),function(x) matrix(rbinom(9,st[x,1:9],prob = S),1,9,byrow = T)))
+  M <- cbind(a,a2[,1],apply(a2[,2:3],1,sum),0,a2[,4],apply(a2[,5:6],1,sum),a2[,7],0,0) # apply(a2[,8:9],1,sum)
+  colnames(M) <- c("F0","F1","Fad","M0","M1","Mad","YY0","YY1","YYad")
+  # YY influence
+  m <- apply(M[,c("YY0","YY1","YYad")],1,sum)
+  ni <- apply(M[,c("M1","Mad")],1,sum)
+  k <- apply(M[,c("F1","Fad")],1,sum)
+  k[which(k > (m + ni))] <- (m + ni)[which(k > (m + ni))] # house keeping - says: if there are more females than males...adjust math so it works...i.e., pick n_males instead of n_females
+  tmp <- rhyper(nn = n,m = m,n = ni,k = k)# Number of YY males contributing (hypergeometric = similar to binomial, but withM replacement)
+  YYcont <- tmp/apply(M[,c("F1","Fad")],1,sum) # YY male contribution to eggs
+  YYcont[!is.finite(YYcont)] <- 0# deal with nans (from 0/0)
+  # M[,"YY0",i+1] <- stYY[,1] # Add YY males
+  M[,"F0"] <- M[,"F0"] - round(YYcont*M[,"F0"]) # Take YY male progeny from F0
+  M[,"M0"] <- M[,"M0"] + round(YYcont*M[,"F0"]) # Add to M0
+  male0 <- round(M[,"F0"]/2) # natural Parents - male
+  M[,"M0"] <- M[,"M0"] + male0
+  M[,"F0"] <- M[,"F0"] - male0
+  # Suppress
+  M[,1:6] <- round(f_suppress(M[,1:6],sdd,n = n,sup = sup) * M[,1:6])
+  # Add YY males
+  M[,"YY0"] <- YYN
+  # YY surv to spawn
+  a3 <- t(sapply(1:nrow(M),function(x) matrix(rbinom(4,c(YYN,M[x,7:9]),prob = YYS),1,4,byrow = T)))
+  M[,7:9] <- cbind(a3[,1],a3[,2],apply(a3[,3:4],1,sum))
+  M[M < 0] <- 0
+  M
 }
-out <- array(dim = c(n,3,z),dimnames = list(NULL,c("0","1","ad"),paste0("T",1:z)))
-out[,,1] <- g(n,matrix(st,1,3),m)
 
-for(i in 1:(z-1)) {
-  out[,,i+1] <- g(n,out[,,i],m)
-}
-#############
-### plots ###
-#############
-# by age
-age <- 0
-plot(1:z,out[1,age + 1,],type = "l",xlim = c(1,50),ann = F,xaxs = "i")
-sapply(2:n,function(x) lines(1:100,out[x,age + 1,]))
-lines(apply(out[,age + 1,],2,mean),col = "red")
-# all
-tmp <- apply(out,c(1,3),sum)
-plot(1:z,tmp[1,],type = "l",xlim = c(0,100),xaxs = "i")
-invisible(sapply(2:n,function(x) lines(1:100,tmp[x,])))
-lines(apply(tmp,2,mean),col = "red")
-#
-apply(tmp,2,median)
+for(j in 1:(z-1)) out[,,j+1] <- f(st = out[,,j],n = n,S = S,Fc = Fc, YYS = SYY)
 
-
-
-
-########################################### Fecundity worksheet
-# Scratch
-1.5 * 600
-# N * SexR * F * Sur
-600*.5*200*.015 # if F = 1.5 thrn s0 == .015 when F = 200 ---------- Need to add another parameter to account for 1 year olds not as fecund or mature as adults.
-1.5/(600/200*2) # do some algebra and figure out formula to get 1.5
-600 = 2*200*1.5*x
-x = 2*200*1.5
-
-
-# parameters for F = 200
-.023 # Egg/age0 Survival
-0.6521739 # penalty Fec for being 1
-
-
-# Proof
-Sex <- .5
-N <- 3000
-Fec <- 200
-S0 <- .023
-Penal <- 0.6521739
-
-N*2.3
-N*1.5
-N*Sex*Fec*S0
-N*Sex*Fec*S0*Penal
-(N*Sex*Fec*S0)/N
-(N*Sex*Fec*S0*Penal)/N
-#
+f(out[,,1],n,S,Fc,YYS)
